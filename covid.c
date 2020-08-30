d8d985dd0655ded971ed6c863384a94caa21c122
fearalert <67753397+fearalert@users.noreply.github.com>
2020-08-30 13:50:18 +0545

    Create covid.c

---
+++ covid.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+struct covid
+{
+    int total_cases,total_active,total_recovered;
+    char district[25];
+}covid[100],t;
+int main()
+{
+    int i,j,n;
+    int date;
+    char month[20];
+    printf("Enter the date of data:\n");
+    scanf("%d %s",&date,&month);
+    printf("Enter the no of covid cases to be stored:\n");
+    scanf("%d",&n);
+    printf("Enter covid cases in the pattern of  district , total cases, total active cases, total recovered cases\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%s %d %d %d",covid[i].district,&covid[i].total_cases,&covid[i].total_active,&covid[i].total_recovered);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(covid[j].total_cases<covid[j+1].total_cases)
+            {
+                t=covid[j];
+                covid[j]=covid[j+1];
+                covid[j+1]=t;
+            }
+        }
+    }
+    printf("\n The date of data collection is %d %s,2020:\n",date,month);
+    printf("\nCovid cases sorted in the order of total no of cases\n");
+    printf("\nDistrict\t\tTotal Cases(s)\t\t Total Active cases \t\t Total recovered cases \n");
+    printf("-------------------------------------------------------------------------------------------------------\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%s\t\t\t\t%d\t\t\t%d\t\t\t\t%d\n",covid[i].district,covid[i].total_cases,covid[i].total_active,covid[i].total_recovered);
+    }
+}
